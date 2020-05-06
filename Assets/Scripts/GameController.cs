61ebc6bc84a84046dc4b0712787cf3794623a80a
Luiz Henrique Barbosa <luiz.hrb2@gmail.com>
2020-05-05 23:00:32 -0300

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,253 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+/// <summary>
+/// Does general operations that are global, such as calling minimax algo, converting the board from int to string, restarting the game, etc...
+/// </summary>
+public class GameController : MonoBehaviour
+{
+
+    public GameObject[] positionArr;
+    public static string playersChoice = "";
+    private Dictionary<string, bool> gameBoard; //TODO: fill this with the gameboard postions and if they are clicked - send for minimax
+    public static Dictionary<BoardPossibleWinScenarios, int> playerBoard = new Dictionary<BoardPossibleWinScenarios, int>();
+    public static Dictionary<BoardPossibleWinScenarios, int> opponentBoard = new Dictionary<BoardPossibleWinScenarios, int>();
+    public enum BoardPossibleWinScenarios { firstRow, secondRow, thirdRow, firstColumn, secondColumn, thirdColumn, forwardDiagonal, backDiagonal };
+    /// <summary>
+    /// Used to map strings to int to convert the board from a data structure of Strings to int and otherwise
+    /// </summary>
+    private Dictionary<string, int> boardIntegerStringMapper = new Dictionary<string, int>
+        {
+            { "[0,0]", 0 }, { "[0,1]", 1 }, { "[0,2]", 2 },
+            { "[1,0]", 3 }, { "[1,1]", 4 }, { "[1,2]", 5 },
+            { "[2,0]", 6 }, { "[2,1]", 7 }, { "[2,2]", 8 },
+        };
+
+    void Awake()
+    {
+        InitBoard();
+        InitializePlayersScore();
+        if (Application.loadedLevel.Equals(1))
+        {
+            RollStart();
+        }
+    }
+
+    private void InitBoard()
+    {
+        positionArr = GameObject.FindGameObjectsWithTag("Position");
+    }
+
+    private void RollStart()
+    {
+        IEnumerable<int> playerRange;
+        int rInt;
+        GenerateRandomNumber(out playerRange, out rInt);
+
+        if (playerRange.Contains(rInt))
+        {
+            Debug.Log("player");
+        }
+        else
+        {
+            Debug.Log("AI");
+        }
+    }
+
+    private static void GenerateRandomNumber(out IEnumerable<int> playerRange, out int rInt)
+    {
+        playerRange = Enumerable.Range(0, 50);
+        IEnumerable<int> aiRange = Enumerable.Range(51, 49);
+        System.Random r = new System.Random();
+        rInt = r.Next(0, 100);
+    }
+
+    public void InitializePlayersScore()
+    {
+        int score = 0;
+        foreach (BoardPossibleWinScenarios boardPossibleWinScenario in Enum.GetValues(typeof(BoardPossibleWinScenarios)))
+        {
+            playerBoard.Add(boardPossibleWinScenario, score);
+            opponentBoard.Add(boardPossibleWinScenario, score);
+        }
+    }
+
+    //TODO: finish, remember to check for tie as well !
+    public void IsGameOver()
+    {
+        bool isGameOver = false;
+        foreach (KeyValuePair<BoardPossibleWinScenarios, int> boardPosition in playerBoard)
+        {
+            if (playerBoard[boardPosition.Key].Equals(3))
+            {
+                Debug.Log("player win");
+                isGameOver = true;
+            }
+        }
+        foreach (KeyValuePair<BoardPossibleWinScenarios, int> boardPosition in opponentBoard)
+        {
+            if (opponentBoard[boardPosition.Key].Equals(3))
+            {
+                Debug.Log("opponent win");
+                isGameOver = true;
+            }
+        }
+        int isTie = 0;
+        foreach (GameObject o in positionArr)
+        {
+            Position parentPosition = o.GetComponent<Position>();
+            isTie = parentPosition.isClicked ? isTie + 1 : isTie;
+        }
+        if (isTie == 9)
+        {
+            Debug.Log("tie");
+            isGameOver = true;
+
+        }
+        if (isGameOver)
+        {
+            RestartGame();
+        }
+    }
+
+    public void RestartGame()
+    {
+        playersChoice = "";
+        //reset wins
+        foreach (BoardPossibleWinScenarios boardPossibleWinScenario in Enum.GetValues(typeof(BoardPossibleWinScenarios)))
+        {
+            playerBoard[boardPossibleWinScenario] = 0;
+            opponentBoard[boardPossibleWinScenario] = 0;
+        }
+        //reset gamobjects
+        for (int i = 0; i < this.positionArr.Length; i++)
+        {
+            positionArr[i].GetComponent<Position>().isClicked = false;
+            for (int j = 0; j < this.positionArr[i].transform.childCount; j++)
+            {
+                GameObject xOrCircle = this.positionArr[i].transform.GetChild(j).gameObject;
+                xOrCircle.GetComponent<ColorControl>().SetColor(xOrCircle.GetComponent<ColorControl>().AlphaColor);
+            }
+        }
+    }
+
+    public void IncrementPositionScore(string positionToIncrement, Dictionary<BoardPossibleWinScenarios, int> board)
+    {
+        switch (positionToIncrement)
+        {
+            case "[0,0]":
+                board[BoardPossibleWinScenarios.firstRow]++;
+                board[BoardPossibleWinScenarios.firstColumn]++;
+                board[BoardPossibleWinScenarios.backDiagonal]++;
+                break;
+            case "[0,1]":
+                board[BoardPossibleWinScenarios.firstRow]++;
+                board[BoardPossibleWinScenarios.secondColumn]++;
+                break;
+            case "[0,2]":
+                board[BoardPossibleWinScenarios.firstRow]++;
+                board[BoardPossibleWinScenarios.thirdColumn]++;
+                board[BoardPossibleWinScenarios.forwardDiagonal]++;
+                break;
+            case "[1,0]":
+                board[BoardPossibleWinScenarios.secondRow]++;
+                board[BoardPossibleWinScenarios.firstColumn]++;
+                break;
+            case "[1,1]":
+                board[BoardPossibleWinScenarios.secondRow]++;
+                board[BoardPossibleWinScenarios.secondColumn]++;
+                board[BoardPossibleWinScenarios.forwardDiagonal]++;
+                board[BoardPossibleWinScenarios.backDiagonal]++;
+                break;
+            case "[1,2]":
+                board[BoardPossibleWinScenarios.secondRow]++;
+                board[BoardPossibleWinScenarios.thirdColumn]++;
+                break;
+            case "[2,0]":
+                board[BoardPossibleWinScenarios.thirdRow]++;
+                board[BoardPossibleWinScenarios.firstColumn]++;
+                board[BoardPossibleWinScenarios.forwardDiagonal]++;
+                break;
+            case "[2,1]":
+                board[BoardPossibleWinScenarios.thirdRow]++;
+                board[BoardPossibleWinScenarios.secondColumn]++;
+                break;
+            case "[2,2]":
+                board[BoardPossibleWinScenarios.thirdRow]++;
+                board[BoardPossibleWinScenarios.thirdColumn]++;
+                board[BoardPossibleWinScenarios.backDiagonal]++;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Plays the AI turn
+    /// </summary>
+    public void PlayAiTurn(GameObject gameObject)
+    {
+        int aiPlayChoice = GameController.playersChoice.Equals("X") ? -1 : 1;
+
+        MiniMax minimaxAlgo = new MiniMax();
+        EndTurnPosition bestPositionToEndTurn = minimaxAlgo.GetBestPosition(aiPlayChoice, ConvertBoardToInt(gameObject), aiPlayChoice, 0, 0);
+        int newBestPositionToPick = bestPositionToEndTurn.position;
+
+        foreach (GameObject boardPositions in this.positionArr)
+        {
+            foreach (KeyValuePair<string, int> integerStringMapperEntry in boardIntegerStringMapper)
+            {
+                if (integerStringMapperEntry.Key == boardPositions.name && integerStringMapperEntry.Value == newBestPositionToPick)
+                {
+                    if (GameController.playersChoice != boardPositions.transform.GetChild(0).gameObject.name)
+                    {
+                        gameObject.GetComponent<MouseEvents>().renderPositionClicked(boardPositions.transform.GetChild(0).gameObject);
+                        gameObject.GetComponent<ColorControl>().RenderAiOtherChoiceInvisible(boardPositions);
+                    }
+                    else
+                    {
+                        gameObject.GetComponent<MouseEvents>().renderPositionClicked(boardPositions.transform.GetChild(1).gameObject);
+                        gameObject.GetComponent<ColorControl>().RenderAiOtherChoiceInvisible(boardPositions);
+                    }
+                }
+            }
+        }
+        minimaxAlgo = null;
+    }
+
+    /// <summary>
+    /// Converts the board from strings of [x,y] to integers for the minimax algo
+    /// </summary>
+    private int[] ConvertBoardToInt(GameObject gameObject)
+    {
+        int[] boardAsIntegers = new int[9];
+        Color32 invisibleColor = gameObject.GetComponent<ColorControl>().InvisibleColor;
+        foreach (GameObject position in this.positionArr)
+        {
+            foreach (KeyValuePair<string, int> integerStringMapperEntry in boardIntegerStringMapper)
+            {
+                if (integerStringMapperEntry.Key == position.name)
+                {
+                    if (!position.GetComponent<Position>().isClicked)
+                    {
+                        boardAsIntegers[integerStringMapperEntry.Value] = 0;
+                    }
+                    else
+                    {
+                        if (!position.transform.GetChild(0).GetComponent<ColorControl>().GetColor().Equals(invisibleColor) && position.transform.GetChild(0).gameObject.name == "X")
+                        {
+                            boardAsIntegers[integerStringMapperEntry.Value] = 1;
+
+                        }
+                        else
+                        {
+                            boardAsIntegers[integerStringMapperEntry.Value] = -1;
+                        }
+                    }
+                }
+            }
+        }
+        return boardAsIntegers;
+    }
+
+}
---
+++ Assets/Scripts/MiniMax.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+public class MiniMax
+{
+
+    public EndTurnPosition GetBestPosition(int calculateBestRouteFor, int[] gameBoard, int calculateIsWinFor, int position, int depth)
+    {
+        EndGameScenario isGameOver = IsGameOver(gameBoard, calculateIsWinFor);
+        if (isGameOver == EndGameScenario.Lose && depth == 2)   //to stop player from winning on his first move - trick the AI to think he won with the best possible Position to prevent opponent from winning
+        {
+            return new EndTurnPosition(EndGameScenario.Win, 2, position);    //if it doesnt work give it lowest position like -1
+        }
+        else if (isGameOver == EndGameScenario.Win)
+        {
+            return new EndTurnPosition(EndGameScenario.Win, depth, position);
+        }
+        else if (isGameOver == EndGameScenario.Lose)
+        {
+            return new EndTurnPosition(EndGameScenario.Lose, depth, position);
+        }
+        else if (IsBoardFull(gameBoard))
+        {
+            return new EndTurnPosition(EndGameScenario.Tie, depth, position);
+        }
+        depth++;
+
+        EndTurnPosition bestPosition = new EndTurnPosition();
+        for (int i = 0; i < gameBoard.Length; i++)
+        {
+            if (gameBoard[i] == 0)
+            {
+                gameBoard[i] = calculateBestRouteFor;
+                //invert calculation between the opponents, clone the board so it wont reflect in other positions
+                EndTurnPosition isPositionWin = GetBestPosition(calculateBestRouteFor * -1, (int[])gameBoard.Clone(), calculateIsWinFor, i, depth);
+                if ((isPositionWin.isWin >= bestPosition.isWin && isPositionWin.depth <= bestPosition.depth))
+                {
+                    bestPosition.position = isPositionWin.position;
+                    bestPosition.isWin = isPositionWin.isWin;
+                    bestPosition.depth = isPositionWin.depth;
+                }
+                gameBoard[i] = 0;
+            }
+        }
+        depth--;
+        return bestPosition;
+    }
+
+    private bool IsBoardFull(int[] gameBoard)
+    {
+        foreach (int gameNode in gameBoard)
+        {
+            if (gameNode == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the current status of the game board is an end game
+    /// </summary>
+    private EndGameScenario IsGameOver(int[] gameBoard, int calculateBestRouteFor)
+    {
+        //win = 1, lose = -1, tie =0
+        //x == 1, o == -1, tie == 0
+
+        int isWinRow = 0;
+        int isWinForwardDiagonal = 0;   // /
+        int isWinBackDiagonal = 0;      // \
+        int rowNumber = 1;
+        int rowSize = (int)Math.Sqrt(gameBoard.Length);
+        int[] isWinColumnArr = new int[rowSize];
+        int winCondition = CreateWinCondition(calculateBestRouteFor);
+        int loseCondition = winCondition * -1;
+
+        for (int i = 0; i < gameBoard.Length; i++)
+        {
+            rowNumber = IsNewRow(rowSize, i) ? ++rowNumber : rowNumber;
+            isWinRow = ResetWinConditionForRow(isWinRow, rowSize, i);
+
+            CalculateIsColumnWin(gameBoard, rowSize, isWinColumnArr, i);
+            isWinRow = CalculateIsRowWin(gameBoard, isWinRow, i);
+            isWinBackDiagonal = CalculateIsBackDiagonalWin(gameBoard, isWinBackDiagonal, rowNumber, rowSize, i);
+            isWinForwardDiagonal = CalculateIsForwardDiagonalWin(gameBoard, isWinForwardDiagonal, rowNumber, rowSize, i);
+
+            if (isWinForwardDiagonal == winCondition || isWinBackDiagonal == winCondition || isWinRow == winCondition || isWinColumnArr[i % rowSize] == winCondition)
+            {
+                return EndGameScenario.Win;
+            }
+            else if (isWinForwardDiagonal == loseCondition || isWinBackDiagonal == loseCondition || isWinRow == loseCondition || isWinColumnArr[i % rowSize] == loseCondition)
+            {
+                return EndGameScenario.Lose;
+            }
+        }
+        return EndGameScenario.Tie;
+    }
+
+    private void CalculateIsColumnWin(int[] gameBoard, int rowSize, int[] isWinColumnArr, int i)
+    {
+        isWinColumnArr[i % rowSize] += gameBoard[i];
+    }
+
+    private int CalculateIsRowWin(int[] gameBoard, int isWinRow, int i)
+    {
+        isWinRow += gameBoard[i];
+        return isWinRow;
+    }
+
+    private int CalculateIsForwardDiagonalWin(int[] gameBoard, int isWinForwardDiagonal, int rowNumber, int rowSize, int i)
+    {
+        if (i != 0 && i == ((rowSize - 1) * rowNumber))
+        {
+            isWinForwardDiagonal += gameBoard[i];
+        }
+        return isWinForwardDiagonal;
+    }
+
+    private int CalculateIsBackDiagonalWin(int[] gameBoard, int isWinBackDiagonal, int rowNumber, int rowSize, int i)
+    {
+        if ((i == ((rowSize + 1) * (rowNumber - 1))))
+        {
+            isWinBackDiagonal += gameBoard[i];
+        }
+        return isWinBackDiagonal;
+    }
+
+    private int ResetWinConditionForRow(int isWinRow, int rowSize, int i)
+    {
+        if (IsNewRow(rowSize, i))
+        {
+            isWinRow = 0;
+        }
+        return isWinRow;
+    }
+
+    private bool IsNewRow(int rowSize, int i)
+    {
+        return i != 0 && (i % rowSize) == 0;
+    }
+
+    private int CreateWinCondition(int calculateBestRouteFor)
+    {
+        int win = 3;
+        if (calculateBestRouteFor == -1)
+        {
+            win *= calculateBestRouteFor;
+        }
+        return win;
+    }
+
+}
